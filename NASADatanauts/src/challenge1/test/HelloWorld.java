316c17404069a56e8750f272619a3aae7cf1a21e
jknight001 <jknight0014@yahoo.com>
2016-07-22 13:03:54 -0700

    Another test

--- NASADatanauts/src/challenge1/test/HelloWorld.java
+++ NASADatanauts/src/challenge1/test/HelloWorld.java
@@ -1,10 +1,11 @@
 package challenge1.test;
+//git@github.com:jknight001/NASADatanauts.git
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello wonderful world!");
+		System.out.println("Hello wonderful beautiful world!");
 
 	}
 
